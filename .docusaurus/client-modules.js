9a1af2f75c283afb147ec0f3cebc7bd56c3af605
Jordan <jordan@jpdesigning.com>
2022-12-22 11:59:40 -0500

    docs: run clean

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/jordan/Desktop/cypress/cypress-heroes-workshop/guide/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/jordan/Desktop/cypress/cypress-heroes-workshop/guide/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/jordan/Desktop/cypress/cypress-heroes-workshop/guide/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/jordan/Desktop/cypress/cypress-heroes-workshop/guide/src/css/custom.css'),
-];
