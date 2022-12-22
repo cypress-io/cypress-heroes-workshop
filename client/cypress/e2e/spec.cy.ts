623e928009cb81f87a848aec7ae2145ae2ce33d4
Jordan <jordan@jpdesigning.com>
2022-12-22 16:54:42 -0500

    feat: lab3 starting point

--- client/cypress/e2e/spec.cy.ts
+++ client/cypress/e2e/spec.cy.ts
@@ -1,13 +1,8 @@
 describe('Hiring A Hero', () => {
   
   beforeEach(() => {
-    // visit the app
     cy.visit('/')
-    // login
-    cy.get('[data-cy=login]').contains('Login').click()
-    cy.get('input[type=email]').type('test@test.com')
-    cy.get('input[type=password]').type('test123')
-    cy.get('button').contains('Sign in').click()
+    cy.login()
   })
 
   it('can hire the Smoker', () => {
@@ -21,14 +16,72 @@ describe('Hiring A Hero', () => {
         // validate that we have the correct hero
         cy.get("h5").contains('The Smoker')
         // click the hire button
-        cy.get('[data-cy=hire]').click()
+        cy.getBySel('hire').click()
     })
 
     // Because this opens a modal we need to break outside of the .within
     // This makes sure that the confirmation modal that appears is once again for the smoker
-    cy.get('[data-cy=confirm-hero-name').contains('The Smoker')
+    cy.getBySel('confirm-hero-name').contains('The Smoker')
 
     // click the confirm hire button
-    cy.get('[data-cy=confirm-hire]').click()
+    cy.getBySel('confirm-hire').click()
+  })
+
+  it('increments the count when liking and hiring', () => {
+    // we need a javascript variable to store the value of saves
+    let saves = 0
+
+    cy.get('app-hero-list>ul')
+      // this gets the list items <li>
+      .children()
+      // this gets the very last list item
+      .last()
+      // this scopes the subsequent cypress commands to within this element
+      .within(() => {
+        // get the number of current fans
+        cy.getBySel('fans')
+          // invokes a function on the yielded subject (element)
+          .invoke('text')
+          // convert that value as a string to a Number
+          .then(Number)
+          // then create a function to work with this number
+          .then(n => {
+            // click the like button
+            cy.getBySel('like').click()
+            // get the number of current fans now
+            cy.getBySel('fans')
+              // make sure the new number is 1 more than the previous
+              .contains(
+                // Need to convert it back to a string
+                String(n + 1)
+              )
+          })
+        
+        // do the same thing for saves
+        cy.getBySel('saves')
+          .invoke('text')
+          .then(Number)
+          .then(n => {
+            // assign the saves variable we previously created to the current value of saves in the DOM
+            saves = n
+          })
+        // click the hire button
+        cy.getBySel('hire').click()
+      })
+
+      // we have to leave the context of the last hero item to confirm the hire
+      cy.getBySel('confirm-hero-name').contains('Fly Girl')
+      cy.getBySel('confirm-hire').click()
+
+      // we need to reestablish the context of the last hero
+      cy.get('app-hero-list>ul').children().last().within(() => {
+        // get the number of saves now in the DOM
+        cy.getBySel('saves')
+          // make sure the new number is 1 more than the previous
+          .contains(
+            // Need to convert it back to a string
+            String(saves + 1)
+          )
+      })
   })
 })
--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -1,37 +1,17 @@
-/// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+declare namespace Cypress {
+    interface Chainable {
+        getBySel(selector: string): Chainable
+        login(email?: string, password?: string): void
+    }
+}
+
+Cypress.Commands.add('getBySel', (selector: string, ...args): Cypress.Chainable => {
+    return cy.get(`[data-cy=${selector}]`, ...args)
+})
+
+Cypress.Commands.add('login', (email = 'test@test.com', password = 'test123'): void => {
+     cy.getBySel('login').contains('Login').click()
+     cy.get('input[type=email]').type('test@test.com')
+     cy.get('input[type=password]').type('test123')
+     cy.get('button').contains('Sign in').click()
+})
